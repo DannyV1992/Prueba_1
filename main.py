feb2c580b574a20e0438463553e1ef9061930986
DannyV1992 <dava01cr@gmail.com>
2024-02-22 17:23:50 -0600

    Cambios

---
+++ main.py
@@ -0,0 +1,9 @@
+def suma(num_1: int, num_2: int) -> int:
+    return num_1 + num_2
+
+def resta(num_1: int, num_2: int) -> int:
+    return num_1 - num_2
+
+if __name__ == '__main__':
+    print(suma(num_1=1, num_2=10))
+    print(resta(num_1=10, num_2=2))
